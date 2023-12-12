30bb0f1d0fbfe4c9dd2f6f171e4120896ac106d1
Le-Yo <deoleyo@gmail.com>
2023-12-12 16:35:33 -0600

    item css

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,11 +12,13 @@ function ExpenseItem(props) {
   return (
     <li>
     <Card className="expense-item">
+    <div className="expense-item__description">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
+     
         <h2>{props.title}</h2>
+       
         <div className="expense-item__price">${props.amount}</div>
-      </div>
+        </div>
      
     </Card>
     </li>
